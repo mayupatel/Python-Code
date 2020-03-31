7852c15360b31a56bfa8bd1f11c38ca10e80f4fd
Mayuri <mpate131@uncc.edu>
2020-03-30 21:36:46 -0400

    python code

--- mpate131_fastaAndGC.py
+++ mpate131_fastaAndGC.py
@@ -1,13 +1,19 @@
 
+NAME: MAYURI PATEL, EMAIL: mpate131@uncc.edu
 
+#importing the module random.        
 import random
 
-#Function returns a) Gene name b) GeneID c) ProteinID
+
+# this function takes header as parameter.
+#Function reports a) Gene name b) GeneID c) ProteinID
 def header_file(fastaHeader):
+    '
 	#1. Parse the header string for gene name.  
 	geneName = fastaHeader.strip().split(" ")[1].split("=")[1].strip("]")
 	print("The Gene Name is: " + geneName)
 
+    
 	#parsing header for gene id.
 	geneID = ""
 	fastaHeaderSplit = fastaHeader.split()[2].split(",")
@@ -16,17 +22,19 @@ def header_file(fastaHeader):
 			geneID = fastaHeaderSplit[gene].split(":")[1].strip("]")
 	print("The GeneID is: " + str(geneID))
 
+    
 	#parsing header for protien id.
 	proteinID = fastaHeader.strip().split(" ")[-2].split("=")[1].strip("]")
 	print("The ProteinID is: " + proteinID)
 
-	#exit the function and return the given variables
-	return geneName,geneID,proteinID
+	#exit the function
+    
 
-
-#function return/report a) length of sequence b) GC content c) Reverse complement sequence
+#this function takes sequence as parameter
+#function reports a) length of sequence b) GC content c) Reverse complement sequence
 def sequence_record(seqRecord):
-	#2. Parse the sequence record, 
+    
+	#2. Parse the sequence record for its length.
     total = len(seqRecord)
     print("This is the total length of the sequence: " + str(total))
 
@@ -39,8 +47,7 @@ def sequence_record(seqRecord):
     reverseComplement = seqRecord[::-1]
     print("This is the reverse complement of the sequence:" + str(reverseComplement))
 
-    #exit the function and return the given variables
-    return total,GCcontent,reverseComplement
+    #exit the function! 
 
 
 #function returns the new mutated string
@@ -56,33 +63,38 @@ def nucleotide(nucleotideSeq, mutation):
 			exit()
 
 	nucleotideSeq = nucleotideSeq.lower() # lowercase string
-	#print(nucleotideSeq)
+
 	newNucleotide = ""
-	#print("the index position starts at " + str(0))
+    
+	#the index position starts at 0
 	for mutationResult in range(mutation):
 		position1 = random.randint(0, len(nucleotideSeq)-1)# generated first random number
-		firstRandom = nucleotideSeq[position1]
+		firstRandom = nucleotideSeq[position1] # select the character of it
 		print("The random position is: " + str(position1) + " and the nucleotide present is: "
 			+ firstRandom)
 
 		nucleotideList = list(nucleotideSeq)# string converted to list
 		position2 = random.randint(0, len(nucleotideList)-1)# generated second random number
-		secondRandom = nucleotideList[position2]
+		secondRandom = nucleotideList[position2] # select the character of it
 		print("The another nucleotide selected: " + secondRandom)
 
-		nucleotideList[position1] = secondRandom.upper() 
+		nucleotideList[position1] = secondRandom.upper() # upper case
 		newNucleotide = newNucleotide.join(nucleotideList)# list is converted to string
 		print(newNucleotide)
 
 		mutatedSeq = newNucleotide.upper()# new mutated sequence 
 		print(mutatedSeq)
 
-	return mutatedSeq
+    #exit the function!
 
 
+#this function takes sequence to mutate by transition substitution.    
 def transitionSeq(nucleotideSeq,mutation):
+    
     #transitions = set([('A', 'G'), ('G', 'A'), ('C', 'T'), ('T', 'C')])
+    
     newNucleotide = ""
+    
     for mutationResult in range(mutation):
     	position1 = random.randint(0, len(nucleotideSeq)-1)# generated first random number
     	firstRandom = nucleotideSeq[position1]
@@ -92,35 +104,52 @@ def transitionSeq(nucleotideSeq,mutation):
     	secondRandom = nucleotideList[position2]
     	print("The another nucleotide selected: " + secondRandom)
 
+        # this causes the substitution of the nucleotide.
     	if firstRandom in ("A", "G") and secondRandom in( "A" , "G"):
     		nucleotideSeq[position1] = secondRandom
     		
     	if firstRandom is "C" or "T" and secondRandom is "C" or "T":
     		nucleotideSeq[position1] = secondRandom
 
-    	#nucleotideList[position1] = secondRandom
+
     	print(nucleotideList)
     	newNucleotide = newNucleotide.join(nucleotideList)# list is converted to string
     	print(newNucleotide)
-    	
+    
+    #exit the function!
 
+
+        
+#this main finction handles the flow of the code.
 def main():
+    
+    #using this fastaheader to parse and other commented header are for validating the written code.
 	fastaHeader = ">lcl|NG_005905.2_cds_NP_009225.1_1 [gene=BRCA1] [db_xref=CCDS:CCDS11453.1,GeneID:672,LRG:p1] [protein=breast cancer type 1 susceptibility protein isoform 1] [exception=annotated by transcript or proteomic data] [protein_id=NP_009225.1] [gbkey=CDS]"
+    
 	#fastaHeader = ">lcl|NC_000007.14_cds_XP_011514170.1_1 [gene=ELN] [db_xref=GeneID:2006] [protein=elastin isoform X1] [protein_id=XP_011514170.1] [gbkey=CDS]"
 	#fastaHeader = ">lcl|NC_000017.11_cds_NP_000537.3_1 [gene=TP53] [db_xref=CCDS:CCDS11118.1,GeneID:7157] [protein=cellular tumor antigen p53 isoform a] [protein_id=NP_000537.3] [gbkey=CDS]"
 	#fastaHeader = ">lcl|NC_000011.10_cds_NP_000509.1_1 [gene=HBB] [db_xref=CCDS:CCDS7753.1,Ensembl:ENSP00000333994.3,GeneID:3043] [protein=hemoglobin subunit beta] [protein_id=NP_000509.1] [gbkey=CDS]"
+    
+    #using the sequence to parse it.
 	seqRecord = "ATGGATTTATCTGCTCTTCGCGTTGAAGAAGTACAAAATGTCATTAATGCTATGCAGAAAATCTTAGAGTGTCCCATCTGTCTGGAGTTGATCAAGGAACCTGTCTCCACAAAGTGTGACCACATATTTTGCAAATTTTG"
 
+    #passing the parameters as header and sequence
+    
 	header_file(fastaHeader) # calling the function
 	sequence_record(seqRecord) # calling the function
+    
 
 	# the third function is called through the input
 	nucleotideSeq = input("Enter the sequence: ")
 	mutation = int(input("How many mutations do you want?: "))
+    
+    #passing the parameters
 	nucleotide(nucleotideSeq, mutation)	# calling the function
 
 	transitionSeq(nucleotideSeq, mutation) # calling the function
 	
+    
+    
 
 if __name__ == "__main__":
 	main()
--- mpate131_pattern.py
+++ mpate131_pattern.py
@@ -1,12 +1,16 @@
 
+NAME: MAYURI PATEL, EMAIL: mpate131@uncc.edu
+        
 
-#function executing the code.
+#this function executing the code.
 def checkarboard(patternInput,patternInput1,characterChoice):
 
-	#assigning character into two variables.
+	#assigning characters into two variables.
 	symbol,symbol1 = characterChoice.split(',')
+    
 	#symbol = "#"
 	#symbol1 = "%"
+    
 	for row in range(0,patternInput):#big box input.
 		#print(row)
 		for smallrow in range(0,patternInput1):#small box input.
@@ -20,19 +24,22 @@ def checkarboard(patternInput,patternInput1,characterChoice):
 						print(symbol1 , end = "")
 
 
-#main function takes the input and call the function.
+                        
+#the main function takes the input and call the function.
 def main():
+    
 	#three input are given for a code to execute.
-	patternInput = int(input("Enter the number used to form a Big Box: "))
+	patternInput = int(input("Enter the number used to form a Big Box: ")) # for big box in pattern
 
-	patternInput1 = int(input("Enter the number used to form a Small Box within the Big Box: "))
+	patternInput1 = int(input("Enter the number used to form a Small Box within the Big Box: ")) # for small box in pattern
 
-	characterChoice = input("Enter any two Special Characters(must be comma separated): ")
+	characterChoice = input("Enter any two Special Characters(@#%&$*)(must be comma separated): ") # characters for the pattern
 
 
-	#function taking all the parameters.
+	# calling the function to take all the parameters to execute.
 	checkarboard(patternInput,patternInput1,characterChoice)
 
+    
 
 if __name__ == '__main__':
 	main()
